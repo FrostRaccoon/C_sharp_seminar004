1c854f35f2cd76e4d5d4dc61f4672447d7351496
EnotWhyNot <danik_maltsev@mail.ru>
2023-01-25 16:30:03 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// ... которая принимает на вход число и выдаёт кол-во цифр в числе.
+// 456 -> 3
+//78 -> 2
+//89126 -> 5
+
+Console.WriteLine(GetCountNum(new Random().Next(1000, 10000)));
+
+static int GetCountNum(int num)
+{
+    Console.WriteLine(num);
+    int count = 0;
+    while(num > 0)
+    {
+        count++;
+        num /= 10;
+    }
+    return count;
+}
