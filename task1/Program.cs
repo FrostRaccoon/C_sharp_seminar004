64045496ee80b820a8c43e9f0cbada113f68aa96
EnotWhyNot <danik_maltsev@mail.ru>
2023-01-25 16:29:53 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//
+
+
+
+int num1 = 7;
+Console.WriteLine(GetSum(num1));
+Console.WriteLine(GetSum(28));
+
+static int GetSum(int num)
+{
+    int res = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        res += i;
+    }
+    return res;
+}
